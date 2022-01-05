99fa5d7eb07f90d3af74e51acbda4e39e549d96d
Anisur Rahman <62845237+anisurarzu@users.noreply.github.com>
2022-01-05 20:50:30 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function openFunction() {
+  document.getElementById("menu").style.width = "300px";
+}
+function closeFunction() {
+  document.getElementById("menu").style.width = "0px";
+}
+function openSearchField() {
+  document.getElementById("search").style.display = "block";
+  document.getElementById("");
+}
+
+function closeSearchField() {
+  document.getElementById("search").style.display = "none";
+}
+$(document).ready(function () {
+  $("#product-slider").owlCarousel({
+    items: 5,
+    itemsDesktop: [1199, 5],
+    itemsDesktopSmall: [980, 4],
+    itemsMobile: [700, 1],
+    pagination: false,
+    navigationText: ["", ""],
+    autoplay: true,
+  });
+});
+$(document).ready(function () {
+  $("#product-slider-two").owlCarousel({
+    items: 5,
+    itemsDesktop: [1199, 5],
+    itemsDesktopSmall: [980, 4],
+    itemsMobile: [700, 1],
+    pagination: false,
+    navigationText: ["", ""],
+    autoplay: true,
+  });
+});
